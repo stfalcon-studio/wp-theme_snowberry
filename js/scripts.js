421977111d2124fa6b7cf86ef22c9570567d1c1f
Andrey Shkodyak <fluxuator@gmail.com>
2012-05-29 15:07:47 +0300

    Bugfix

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-	$('.entry-content table').each(function(){
-		if($(this).find('tr').length >= 8){
-			$(this).find('tr:odd').addClass('table-row-odd');
-		}
-	});
-	$('#access .menu > ul > li').each(function(){
-		$(this).find('a').append('<span class="top-menu-tail"></span>');
-		if($(this).find('ul.children').length){
-			$(this).addClass('submn');
-			$(this).find('a').append('<span class="menu-arrow-down"></span>')
-		}
-	});
-});
+(function ($) {
+    $(document).ready(function () {
+        $('.entry-content table').each(function () {
+            if ($(this).find('tr').length >= 8) {
+                $(this).find('tr:odd').addClass('table-row-odd');
+            }
+        });
+        $('#access .menu > ul > li').each(function () {
+            $(this).find('a').append('<span class="top-menu-tail"></span>');
+            if ($(this).find('ul.children').length) {
+                $(this).addClass('submn');
+                $(this).find('a').append('<span class="menu-arrow-down"></span>')
+            }
+        });
+    });
+})(jQuery);
